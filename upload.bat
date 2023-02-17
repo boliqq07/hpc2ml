03f53161f8840a6d9f9eebb3020231b9951d1e71
changxin W <986798607@qq.com>
2023-02-17 15:13:50 +0800

    new

--- upload.bat
+++ upload.bat
@@ -2,9 +2,9 @@
 @echo on
 set path=D:\Anaconda3;D:\Anaconda3\Library\bin;D:\Anaconda3\Scripts;D:\Anaconda3\condabin;%path%
 set path=C:\Users\Administrator\anaconda3;C:\Users\Administrator\anaconda3\Library\bin;%path%
-set path=C:\Users\Administrator\anaconda3\Scripts;C:\Users\Administrator\anaconda3;\condabin;%path%
-set path=C:\Users\98679\anaconda3;C:\Users\98679\anaconda3\Library\bin;%path%
-set path=C:\Users\98679\anaconda3\Scripts;C:\Users\98679\anaconda3\condabin;%path%
+set path=C:\Users\Administrator\anaconda3\Scripts;C:\Users\Administrator\anaconda3\condabin;%path%
+set path=C:\ProgramData\Anaconda3;C:\ProgramData\Anaconda3\Library\bin;%path%
+set path=C:\ProgramData\Anaconda3\Scripts;C:\ProgramData\Anaconda3\condabin;%path%
 
 path
 
